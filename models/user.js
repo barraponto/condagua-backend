03383eb6d31d4775c2b7469b5ac6a5cffc9b8584
Capi Etheriel <barraponto@gmail.com>
2018-03-16 12:29:19 -0300

    cria modelo de usuario

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('../mongoose');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
@@ -10,14 +10,16 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateToken = function() {
-  return jwt.sign({ userId: this.id }, JWT_SECRET, { expires_in: JWT_EXPIRY});
+  return jwt.sign({ userId: this.id }, JWT_SECRET, { expiresIn: JWT_EXPIRY});
 };
 
 const User = mongoose.model('User', userSchema);
 
 User.findOrCreate = function(data) {
   return this.findOne(data)
-    then(user => user ? user : this.create(data));
+    .then(user => {
+      return user ? user : this.create(data)
+    });
 }
 
 module.exports = User;
